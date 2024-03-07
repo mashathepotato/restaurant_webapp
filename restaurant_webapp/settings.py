6a443c68b603ebecb3c24451011c8c61adced216
Yihao0118 <2746098c@student.gla.ac.uk>
2024-03-07 17:48:54 +0000

    Merge branch 'main' into Chen-Yihao-work

--- restaurant_webapp/settings.py
+++ restaurant_webapp/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+print(STATIC_DIR)
 
 
 # Quick-start development settings - unsuitable for production
