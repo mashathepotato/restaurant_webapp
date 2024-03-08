74b6cbce07efa043927f29f9f9a8e493842027cc
Yihao0118 <2746098c@student.gla.ac.uk>
2024-03-08 13:30:08 +0000

    finish issue 19

--- food_advisor/urls.py
+++ food_advisor/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('', views.index, name='index'), # No home page url, index is home page.
 
     # Uncomment when appropriate views are created.
-    # path('register/', views.registerUser, name='registerUser'),
+    path('register/', views.registerUser, name='registerUser'),
     # path('register/manager/', views.registerManager, name='registerManager'), # This page also includes restaurant detail entry.
-    # path('signin/', views.user_signin, name='signin'),
+    path('signin/', views.user_signin, name='signin'),
+    path('owner/', views.registerOwner, name='owner'),
     # path('restaurant/<slug:restaurant_id_slug>/', views.show_restaurant, name='showRestaurant'),
     # path('restaurant/<slug:restaurant_id_slug>/manage/', views.manage_restaurant, name="manageRestaurant"),
     # path('signout/', views.user_signout, name='signout'),
